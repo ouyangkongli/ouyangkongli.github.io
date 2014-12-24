ab42da51302516c7ca361a252e4cc2e9846b4a87
oukongli <oukongli@gmail.com>
2014-12-24 10:22:43 +0800

    change id to oukongli refer codepiano

--- js/oukongli.js
+++ js/oukongli.js
@@ -1,8 +1,8 @@
 /* =============================================================================
-#     FileName: codepiano.js
+#     FileName: oukongli.js
 #         Desc: javascript for blog
-#       Author: codepiano
-#        Email: codepiano.li@gmail.com
+#       Author: oukongli
+#        Email: oukongli.li@gmail.com
 #     HomePage: http://www.weibo.com/anyexingchen
 #      Version: 0.0.1
 #   LastChange: 2013-05-12 01:39:30
